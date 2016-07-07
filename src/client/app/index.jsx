4240a7473e83c61be235e721a5ed6ffd8c8d51fb
Jonathan Kvicky <jonkvix@gmail.com>
2016-07-06 17:33:18 -0700

    Removed GitHub accessToken

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 import axios from 'axios';
 import GithubFeed from './components/github-feed';
 
-const accessToken = '8ff8ec416ad206564e5f9cb44cc5ee53999c0a7f';
+const accessToken;
 
 class App extends Component {
 
